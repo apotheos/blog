3728c451c6dae11d4b44af5753d9ea5b38123a0e
Mike Holler <apotheos121@gmail.com>
2013-05-09 20:45:35 -0500

    First commit, created crude CRU interface for posts.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '20af9a79ed1acbb7f03db4a7cca9dd56ed1e7cb9a408d07295f82d668ebe516098cdc36080c1c19d0ed8a4706a5e46deca9f7f8bf38f80ac265f55269b5710e9'
