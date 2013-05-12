602d77b1d699b133d73e0ae55261aba54b9e1a1d
Mike Holler <apotheos121@gmail.com>
2013-05-11 20:50:31 -0500

    Restyled and now with complete CRUD operations.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,35 @@
 class PostsController < ApplicationController
+
+  respond_to :html, :json, :xml
+
   def index
-    @posts = Post.all
+    respond_with(@posts = Post.all)
   end
 
   def show
-    @post = Post.find(params[:id])
+    respond_with(@post = Post.find(params[:id]))
   end
 
   def new
     @post = Post.new
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => @post}
+    end
   end
 
   def create
     @post = Post.new(params[:post])
 
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to(@post, :notice => 'Post was successfully created.')}
+        format.json { render :json => @post, :status => :created, :location => @post}
+      else
+        format.html { render 'new'}
+        format.json { render :json => @post.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
@@ -31,6 +43,15 @@ class PostsController < ApplicationController
       redirect_to @post
     else
       render 'edit'
+    end
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+
+    respond_to do |format|
+      format.html { redirect_to posts_url }
+      format.json { head :no_content }
     end
   end
 end
